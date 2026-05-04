be97d023629c90568f8017f3215f93b75aa99d86
Puff <erwinaston11704@gmail.com>
2026-05-04 20:43:17 +0800

    Refactor surrender.php for improved readability

--- user/surrender.php
+++ user/surrender.php
@@ -161,7 +161,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <a href="dashboard.php"><i class="fa-solid fa-house"></i> Dashboard</a>
         <a href="report.php"><i class="fa-solid fa-flag"></i> Report</a>
         <a href="surrender.php" class="active"><i class="fa-solid fa-hand"></i> Surrender</a>
-        <a href="adopt.php"><i class="fa-solid fa-heart"></i> Adopt</a>
         <a href="events.php"><i class="fa-solid fa-calendar"></i> Events</a>
         <a href="resources.php"><i class="fa-solid fa-book"></i> Resources</a>
         <a href="about.php"><i class="fa-solid fa-circle-info"></i> About</a>
